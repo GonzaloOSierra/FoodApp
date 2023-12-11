0d1fb836e74091a05e6b157fd06bfd0240fe6242
GonzaloOSierra <gonzalosierra1309@gmail.com>
2023-12-11 02:58:34 -0300

    buscador

---
+++ html/views/opening/searcher.php
@@ -0,0 +1,200 @@
+<?php
+include_once "/var/www/html/models/openmodel.php";
+
+$openModel = new OpenModel();
+$openers = $openModel->getAll();
+
+$startDate = isset($_POST["fechaInicio"]) ? $_POST["fechaInicio"] : '';
+$finalDate = isset($_POST["fechaFin"]) ? $_POST["fechaFin"] : '';
+$selectedName = isset($_POST["employee_name"]) ? $_POST["employee_name"] : '';
+
+// Crear un vector con los años en el rango especificado
+$yearsInRange = range(date('Y', strtotime($startDate)), date('Y', strtotime($finalDate)));
+
+// Bucle principal
+foreach ($yearsInRange as $currentYear) {
+    // Filtrar los registros del año actual
+    $filteredOpeners = array_filter($openers, function ($open) use ($startDate, $finalDate, $selectedName, $currentYear) {
+        $start = strtotime($open->getStart_date());
+
+        $nameFilter = empty($selectedName) || $selectedName == $open->getName_employee() . ' ' . $open->getLast_name_employee();
+
+        return ($start >= strtotime($startDate) && $start <= strtotime($finalDate) && $nameFilter && date('Y', $start) == $currentYear);
+    });
+
+    // Agrupa los resultados por semana y calcula el total por semana, por mes y por año
+    $semanas = [];
+    $totalesMesCredit = [];
+    $totalesMesEfective = [];
+    $totalesMesQr = [];
+    $totalesMesDebito = [];
+    $totalesAnioCredit = 0;
+    $totalesAnioEfective = 0;
+    $totalesAnioQr = 0;
+    $totalesAnioDebito = 0;
+    $totalAnio = 0;
+    $anioAnterior = null;
+
+    if (is_array($filteredOpeners)) {
+        $numeroSemanaAnterior = null;
+
+        $html = ''; // Inicializa la variable HTML
+
+        foreach ($filteredOpeners as $open) {
+            $fechaInicio = new DateTime($open->getStart_date());
+            $numeroSemana = $fechaInicio->format('W');
+            $mes = $fechaInicio->format('F');
+            $anio = $fechaInicio->format('o');
+
+            if (!isset($semanas[$numeroSemana])) {
+                // Agrega una fila adicional al cambiar de semana con el total de la semana anterior
+                if ($numeroSemanaAnterior !== null) {
+                    $html .= "<tr class='table-secondary'>
+                    <td colspan='11'>Semana {$numeroSemanaAnterior} - Mes: {$semanas[$numeroSemanaAnterior]['mes']} - Año: {$semanas[$numeroSemanaAnterior]['anio']} - 
+                        Total Semana (Credit): {$semanas[$numeroSemanaAnterior]['total_credit']} - 
+                        Total Semana (Efective): {$semanas[$numeroSemanaAnterior]['total_efective']} - 
+                        Total Semana (QR): {$semanas[$numeroSemanaAnterior]['total_qr']} - 
+                        Total Semana (Debito): {$semanas[$numeroSemanaAnterior]['total_debito']}
+                    </td>
+                    </tr>";
+                }
+
+                // Agrega una fila de encabezado para indicar el año actual
+                if ($anio != $anioAnterior) {
+                    if (!empty($html)) {
+                        $html .= "</table>"; // Cierra la tabla si hay datos anteriores
+                        $html .= "</div>";
+                        echo $html;
+                    }
+
+                    $html = "<div class='year-results'>";
+                    $html .= "<table>";
+                    $html .= "<tr class='table-secondary'><td colspan='11'>Año: $anio</td></tr>";
+                    $anioAnterior = $anio;
+                }
+
+                $semanas[$numeroSemana] = [
+                    'resultados' => [],
+                    'total_credit' => 0,
+                    'total_efective' => 0,
+                    'total_qr' => 0,
+                    'total_debito' => 0,
+                    'mes' => $mes,
+                    'anio' => $anio,
+                ];
+            }
+
+            if (!isset($totalesMesCredit[$mes])) {
+                $totalesMesCredit[$mes] = 0;
+                $totalesMesEfective[$mes] = 0;
+                $totalesMesQr[$mes] = 0;
+                $totalesMesDebito[$mes] = 0;
+            }
+
+            if (!isset($totalesAnioCredit)) {
+                $totalesAnioCredit = 0;
+                $totalesAnioEfective = 0;
+                $totalesAnioQr = 0;
+                $totalesAnioDebito = 0;
+            }
+
+            $semanas[$numeroSemana]['resultados'][] = $open;
+            $semanas[$numeroSemana]['total_credit'] += $open->getAmount_credit_final();
+            $semanas[$numeroSemana]['total_efective'] += $open->getAmount_efective_final();
+            $semanas[$numeroSemana]['total_qr'] += $open->getAmount_qr_final();
+            $semanas[$numeroSemana]['total_debito'] += $open->getAmount_debito_final();
+
+            // Calcula el total del mes
+            $totalesMesCredit[$mes] += $open->getAmount_credit_final();
+            $totalesMesEfective[$mes] += $open->getAmount_efective_final();
+            $totalesMesQr[$mes] += $open->getAmount_qr_final();
+            $totalesMesDebito[$mes] += $open->getAmount_debito_final();
+
+            // Calcula el total del año
+            $totalesAnioCredit += $open->getAmount_credit_final();
+            $totalesAnioEfective += $open->getAmount_efective_final();
+            $totalesAnioQr += $open->getAmount_qr_final();
+            $totalesAnioDebito += $open->getAmount_debito_final();
+            $totalAnio += $open->getAmount_credit_final();
+
+            // Actualiza la semana anterior
+            $numeroSemanaAnterior = $numeroSemana;
+
+            $html .= "<tr>
+            <td>{$open->getId_open()}</td>
+            <td>{$open->getName_employee()} {$open->getLast_name_employee()}</td>
+            <td>{$open->getId_terminal()}</td>
+            <td>{$open->getStart_date()}</td>
+            <td>{$open->getFinal_date()}</td>
+            <td>{$open->getDate_start_E()}</td>
+            <td>{$open->getDate_Final_E()}</td>
+            <td>{$open->getAmount_credit()}</td>
+            <td>{$open->getAmount_credit_final()}</td>
+            <td>{$open->getAmount_efective()}</td>
+            <td>{$open->getAmount_efective_final()}</td>
+            <td>{$open->getAmount_qr()}</td>
+            <td>{$open->getAmount_qr_final()}</td>
+            <td>{$open->getAmount_debito()}</td>
+            <td>{$open->getAmount_debito_final()}</td>
+        </tr>";
+        }
+
+        // Agrega la última fila adicional con el total de la última semana
+        if ($numeroSemanaAnterior !== null) {
+            $html .= "<tr class='table-secondary'>
+            <td colspan='11'>Semana {$numeroSemanaAnterior} - Mes: {$semanas[$numeroSemanaAnterior]['mes']} - Año: {$semanas[$numeroSemanaAnterior]['anio']} - 
+                Total Semana (Credit): {$semanas[$numeroSemanaAnterior]['total_credit']} - 
+                Total Semana (Efective): {$semanas[$numeroSemanaAnterior]['total_efective']} - 
+                Total Semana (QR): {$semanas[$numeroSemanaAnterior]['total_qr']} - 
+                Total Semana (Debito): {$semanas[$numeroSemanaAnterior]['total_debito']}
+            </td>
+            </tr>";
+        }
+
+        // Cierra la tabla si hay datos al final del bucle
+        if (!empty($html)) {
+            $html .= "</table>";
+            $html .= "</div>";
+            echo $html;
+        }
+    } else {
+        echo "No hay resultados.";
+    }
+
+    // Ordena el array de totalesMesCredit de mayor a menor según el total
+    arsort($totalesMesCredit);
+
+    // Muestra el total del mes y del año al final del resultado
+    echo "<div class='year-results'>";
+    echo "<table>";
+    echo "<tr class='table-secondary'><td colspan='11'>";
+
+    foreach ($totalesMesCredit as $mes => $total) {
+        echo "Total de $mes (Credit): $total - ";
+    }
+
+    foreach ($totalesMesEfective as $mes => $total) {
+        echo "Total de $mes (Efective): $total - ";
+    }
+
+    foreach ($totalesMesQr as $mes => $total) {
+        echo "Total de $mes (QR): $total - ";
+    }
+
+    foreach ($totalesMesDebito as $mes => $total) {
+        echo "Total de $mes (Debito): $total - ";
+    }
+
+    echo "</td></tr>";
+
+    // Muestra los totales por año
+    echo "<tr class='table-secondary'><td colspan='11'>";
+    echo "Total del Año $anioAnterior (Credit): $totalesAnioCredit - ";
+    echo "Total del Año $anioAnterior (Efective): $totalesAnioEfective - ";
+    echo "Total del Año $anioAnterior (QR): $totalesAnioQr - ";
+    echo "Total del Año $anioAnterior (Debito): $totalesAnioDebito - ";
+    echo "</td></tr>";
+    echo "</table>";
+    echo "</div>";
+}
+?>
